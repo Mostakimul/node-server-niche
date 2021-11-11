86a253a013f94b72c9258e733f80b88f4e7de243
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-11-11 21:04:08 +0100

    retrive single user and check isAdmin

--- index.js
+++ index.js
@@ -29,6 +29,18 @@ async function run() {
       res.json(result);
     });
 
+    // fecth sigle user
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
     // update user if google sign
     app.put('/users', async (req, res) => {
       const user = req.body;
